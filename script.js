dd14a5e69e485bb1cce4e321943693ac0cacaf9e
kate patch <kate.epatch@gmail.com>
2022-07-03 17:15:08 -0600

    changing to true fasle quiz for simpliciy on my part

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 var startBtn = document.getElementById("start-quiz");
 var answerBtn = document.getElementById("answer-buttons");
 var questionText = document.getElementById('question-ask');
-var timer = 120;
+var timerCount = 120;
 var countDown;
 var a1 = document.getElementById("ans1");
 var a2 = document.getElementById("ans2");
-var a3 = document.getElementById("ans3");
-var a4 = document.getElementById("ans4");
+// var a3 = document.getElementById("ans3");
+// var a4 = document.getElementById("ans4");
 var timerEl = document.getElementsByClass("timer-count");
 
 
@@ -18,8 +18,8 @@ var questions = [
     {
       question: "Commonly used data types DO NOT include:",
       choices: [
-        {text: "strings", correct: false},
-        {text: "booleans", correct: false},
+        // {text: "strings", correct: false},
+        // {text: "booleans", correct: false},
         {text: "alerts", correct: true},
         {text: "numbers", correct: false},
     ]          
@@ -28,8 +28,8 @@ var questions = [
     {
       question: "The condition in an if / else statement is enclosed within ____.",
       choices: [ 
-        {text: "quotes", correct: false},
-        {text: "curly brakcets", correct: false},
+        // {text: "quotes", correct: false},
+        // {text: "curly brakcets", correct: false},
         {text: "parentheses", correct: true},
         {text: "square brackets", correct: false}, 
       ]
@@ -38,10 +38,10 @@ var questions = [
     {
       question: "Arrays in JavaScript can be used to store ____.",
       choices: [
-        {text: "numbers and strings", correct: false},
-        {text: "other arrays", correct: false},
-        {text: "booleans", correct: false},
-        {text: "all of the above", correct: true},
+        {text: "numbers and strings, other arrays, booleans", correct: true},
+        {text: "styles", correct: false},
+        // {text: "booleans", correct: false},
+        // {text: "all of the above", correct: true},
       ]
     },
 
@@ -49,9 +49,9 @@ var questions = [
       question: "String values must be enclosed within ____ when being assigned to variables.",
       choices: [
         {text: "commas", correct: false},
-        {text: "curly brackets", correct: false},
+        // {text: "curly brackets", correct: false},
         {text: "quotes", correct: true},
-        {text: "parentheses", correct: false},
+        // {text: "parentheses", correct: false},
       ]
     },
 
@@ -59,8 +59,8 @@ var questions = [
       question: "A very useful tool used during development and debugging for printing content to the debugger is:",
       choices: [
         {text: "console.log", correct: true},
-        {text: "JavaScript", correct: false},
-        {text: "terminal/bash", correct: false},
+        // {text: "JavaScript", correct: false},
+        // {text: "terminal/bash", correct: false},
         {text: "for loops", correct: false},
       ]
     }
@@ -71,67 +71,91 @@ var questions = [
   function startQuiz() {
     startBtn.classList.add("hide");
     questionText.innerHTML = questions[currentIndex].question;
-    countDown = setInterval(timer, 1000);
-    answerBtn.classList.remove("hide");
-    // randomQuestions = questions.sort(() => Math.random() > .5 ? 1 : -1);
-    currentIndex = 0;
-    showQuestions();
-    nextQuestion();
-    ans1.addEventListener("click", chooseAnswer);
-    ans2.addEventListener("click", chooseAnswer);
-    ans3.addEventListener("click", chooseAnswer);
-    ans4.addEventListener("click", chooseAnswer);
+    a1.innerHTML = questions[currentIndex].choices[0].text;
+    a1.onclick = () => {
+        let ano=0;
+        if(questions[currentIndex].choices[ano].text){
+            if(score<5){
+                score++;
+            }
+        }
+        userScore.innerHTML = score;
+        if(currentIndex<4) {
+            nextQuestion();
+        }
+    }
+    a2.innerHTML = questions[currentIndex].choices[1].text;
+    a2.onclick = () => {
+        let ano=1;
+        if(questions[currentIndex].choices[ano].text) {
+            if(score<5){
+                score++;
+            }
+        userScore.innerHTML = score;
+        if(currentIndex<4){
+            nextQuestion();
+        }
+        }
+    }
+}
+//     countDown = setInterval(timer, 1000);
+//     answerBtn.classList.remove("hide");
+//     // randomQuestions = questions.sort(() => Math.random() > .5 ? 1 : -1);
+//     currentIndex = 0;
+//     showQuestions();
+//     nextQuestion();
+//     ans1.addEventListener("click", chooseAnswer);
+//     ans2.addEventListener("click", chooseAnswer);
+//     // ans3.addEventListener("click", chooseAnswer);
+//     // ans4.addEventListener("click", chooseAnswer);
 
-  }
-function nextQuestion() {
-    if (currentIndex >= questions.length) {
+//   }
+// function nextQuestion() {
+//     if (currentIndex >= questions.length) {
         
-        clearInterval(countDown);
-        submitHighScore();
-    } else {
-        showQuestions(randomQuestion[currentIndex]);
-    }
-}
+//         clearInterval(countDown);
+//         submitHighScore();
+//     } else {
+//         showQuestions(randomQuestion[currentIndex]);
+//     }
+// }
 
-function showQuestions(questions) {
+// function showQuestions(questions) {
     
-    let choices = questions[currentIndex].choices;
-    choices.sort(() => Math.random() > .5 ? 1 : -1);
+//     let choices = questions[currentIndex].choices;
+//     choices.sort(() => Math.random() > .5 ? 1 : -1);
 
-    a1.textContent = choices[0].text;
-    a2.textContent = choices[1].text;
-    a3.textContent = choices[2].text;
-    a4.textContent = choices[3].text;
+//     a1.textContent = choices[0].text;
+//     a2.textContent = choices[1].text;
+//     // a3.textContent = choices[2].text;
+//     // a4.textContent = choices[3].text;
 
-    a1.value = choices[0].correct;
-    a2.value = choices[1].correct;
-    a3.value = choices[2].correct;
-    a4.value = choices[3].correct;
-}
+//     a1.value = choices[0].correct;
+//     a2.value = choices[1].correct;
+//     // a3.value = choices[2].correct;
+//     // a4.value = choices[3].correct;
+// }
 
-function chooseAnswer(event) {
-    correct.classList.add("hide");
-    incorrect.classList.add("hide");
-    if (event.target.value == "true") {
-        currentIndex++;
-        nextQuestion();
-    } else {
-        timer = timer - 5;
-        currentIndex++;
-        nextQuestion();
-    }
-}
+// function chooseAnswer(event) {
+//     if (event.target.value == "true") {
+//         currentIndex++;
+//         nextQuestion();
+//     } else {
+//         timer = timer - 5;
+//         currentIndex++;
+//         nextQuestion();
+//     }
+// }
 
-function timer() {
-   timer--;
-   timerEl.textContent=timer;
-    if (timer <= -1) {
-        clearInterval(countDown);
-        alert("Outta Time!");
-        endQuiz();
-    }
-}
+// function timer() {
+//    timerCount--;
+//    timerEl.textContent=timerCount;
+//     if (timerCount <= -1) {
+//         alert("Outta Time!");
+//         endQuiz();
+//     }
+// }
 
-function endQuiz() {
-    clearInterval(timer);
-}
+// function endQuiz() {
+//     clearInterval(timer);
+// }
